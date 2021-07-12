3cd80d3fc69d98886268473d13398dc0a655207f
Leandro Marques <leandro.marques.oliveira@everis.com>
2021-07-12 09:37:02 -0300

    Improving the Desing of the API to Make It Fluent

--- src/org/paumard/chaining/PlayWithComparators.java
+++ src/org/paumard/chaining/PlayWithComparators.java
@@ -3,8 +3,6 @@ package org.paumard.chaining;
 import org.paumard.chaining.function.Comparator;
 import org.paumard.chaining.model.Person;
 
-import java.util.function.Function;
-
 public class PlayWithComparators {
 
     public static void main(String[] args) {
@@ -13,16 +11,16 @@ public class PlayWithComparators {
         Person john = new Person("John", 22);
         Person linda = new Person("Linda", 26);
         Person james = new Person("James", 32);
+        Person jamesBis = new Person("James", 26);
 
-        Function<Person, String> getName = Person::getName;
-        Function<Person, Integer> getAge = Person::getAge;
+        Comparator<Person> cmp =
+                Comparator.comparing(Person::getName)
+                          .thenComparing(Person::getAge);
 
-        Comparator<Person> cmpName = Comparator.comparing(getName);
-        Comparator<Person> cmpAge = Comparator.comparing(getAge);
-
-        System.out.println("Mary > John : " + (cmpName.compare(mary, john) > 0));
-        System.out.println("Jhon > James : " + (cmpName.compare(john, james) > 0));
-        System.out.println("Linda > John : " + (cmpName.compare(linda, john) > 0));
+        System.out.println("Mary > John : " + (cmp.compare(mary, john) > 0));
+        System.out.println("Jhon > James : " + (cmp.compare(john, james) > 0));
+        System.out.println("Linda > John : " + (cmp.compare(linda, john) > 0));
+        System.out.println("James > James Bis : " + (cmp.compare(james, jamesBis) > 0));
     }
 
 }
--- src/org/paumard/chaining/function/Comparator.java
+++ src/org/paumard/chaining/function/Comparator.java
@@ -17,4 +17,22 @@ public interface Comparator<T> {
         return (p1, p2) -> keyExtractor.apply(p1).compareTo(keyExtractor.apply(p2));
     }
 
+    default <U extends Comparable<? super U>> Comparator<T> thenComparing(Function<T, U> keyExtractor) {
+        Objects.requireNonNull(keyExtractor);
+        Comparator<T> other = comparing(keyExtractor);
+        return this.thenComparing(other);
+    }
+
+    default  Comparator<T> thenComparing(Comparator<T> other) {
+        Objects.requireNonNull(other);
+        return (T t1, T t2) -> {
+            int compare = this.compare(t1, t2);
+            if(compare == 0) {
+                return other.compare(t1, t2);
+            } else {
+                return compare;
+            }
+        };
+    }
+
 }
